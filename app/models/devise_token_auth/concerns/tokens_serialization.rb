1856ff2e9407a63f0dcaea16686f3a419205ed04
cimigree <cindy@zoobean.com>
2020-02-28 12:05:49 -0500

    Adds debugging code.

--- app/models/devise_token_auth/concerns/tokens_serialization.rb
+++ app/models/devise_token_auth/concerns/tokens_serialization.rb
@@ -1,12 +1,15 @@
 module DeviseTokenAuth::Concerns::TokensSerialization
   # Serialization hash to json
   def self.dump(object)
+    puts "CINDY object is #{object.inspect}"
     object.each_value(&:compact!) unless object.nil?
     JSON.generate(object)
   end
 
   # Deserialization json to hash
   def self.load(json)
+    puts "CINDY json is #{json.inspect}" unless json.nil?
+    binding.pry
     case json
     when String
       JSON.parse(json)
